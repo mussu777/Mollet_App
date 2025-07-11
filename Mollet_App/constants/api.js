8d98a01c446fb634e6f88938061f40d9bbf73fa8
mussu777 <mustak65ee@gmail.com>
2025-07-12 00:16:04 +0530

    updated the sign in page , signout and the homepage

---
+++ Mollet_App/constants/api.js
@@ -0,0 +1,11 @@
+// Production
+// export const API_URL = "/api";
+
+// android phone
+export const API_URL = "http://192.168.209.161:3000/api"; 
+
+// android simulator
+// export const API_URL = "http://10.0.2.2:3000/api"; 
+
+
+
