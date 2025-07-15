ade04636e98212a4cdc4e3ba69be440436f62df9
mussu777 <mustak65ee@gmail.com>
2025-07-15 23:23:05 +0530

    changes in Api Url

--- constants/api.js
+++ constants/api.js
@@ -1,8 +1,8 @@
 // Production
-// export const API_URL = "/api";
+export const API_URL = "https://mollet-app-server.onrender.com/api";
 
 // android phone
-export const API_URL = "http://192.168.209.161:3000/api"; 
+// export const API_URL = "http://192.168.209.161:3000/api"; 
 
 // android simulator
 // export const API_URL = "http://10.0.2.2:3000/api"; 
